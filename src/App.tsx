93ee86c85f9a18e979d9fce7896ef1f1cddcd1d3
anixel <45008199+andifg@users.noreply.github.com>
2023-03-05 13:14:00 +0100

    center main part in web browser

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import CoffeeApp from './components/CoffeeApp';
 
 function App() {
   return (
-    <Row >
-    <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12} >
+    <Row justify="center" align="middle">
+    <Col className='App-header' xs={24} sm={24} md={24} lg={12} xl={12} xxl={12} >
       <div >
       <CoffeeApp />
       </div>
