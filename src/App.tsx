3b444e54e7ebd6d68db39096c7f5f384aac653b5
anixel <45008199+andifg@users.noreply.github.com>
2023-08-18 11:04:34 +0200

    empty vite setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Row, Col } from "antd";
-import CoffeeApp from "./components/CoffeeApp";
-
-import { OpenAPI } from "./client";
-
-OpenAPI.BASE = process.env.BACKEND_HOST_URL || "http://localhost:8000";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Row justify="center" align="middle">
-      <Col
-        className="App-header"
-        xs={24}
-        sm={24}
-        md={24}
-        lg={12}
-        xl={12}
-        xxl={12}
-      >
-        <div>
-          <CoffeeApp />
-        </div>
-      </Col>
-    </Row>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
