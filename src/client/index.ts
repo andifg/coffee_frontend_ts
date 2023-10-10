5480f1032c7102f49c89a46241fe8171d6a6de86
anixel <45008199+andifg@users.noreply.github.com>
2023-10-10 10:38:27 +0200

    fixup! update client

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,21 +2,21 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export { ApiError } from './core/ApiError';
-export { CancelablePromise, CancelError } from './core/CancelablePromise';
-export { OpenAPI } from './core/OpenAPI';
-export type { OpenAPIConfig } from './core/OpenAPI';
+export { ApiError } from "./core/ApiError";
+export { CancelablePromise, CancelError } from "./core/CancelablePromise";
+export { OpenAPI } from "./core/OpenAPI";
+export type { OpenAPIConfig } from "./core/OpenAPI";
 
-export type { Body__create_image_api_v1_coffees__coffee_id__image_post } from './models/Body__create_image_api_v1_coffees__coffee_id__image_post';
-export type { Coffee } from './models/Coffee';
-export type { HealthStatus } from './models/HealthStatus';
-export type { HTTPValidationError } from './models/HTTPValidationError';
-export type { Rating } from './models/Rating';
-export type { RatingSummary } from './models/RatingSummary';
-export type { UpdateCoffee } from './models/UpdateCoffee';
-export type { ValidationError } from './models/ValidationError';
+export type { Body__create_image_api_v1_coffees__coffee_id__image_post } from "./models/Body__create_image_api_v1_coffees__coffee_id__image_post";
+export type { Coffee } from "./models/Coffee";
+export type { HealthStatus } from "./models/HealthStatus";
+export type { HTTPValidationError } from "./models/HTTPValidationError";
+export type { Rating } from "./models/Rating";
+export type { RatingSummary } from "./models/RatingSummary";
+export type { UpdateCoffee } from "./models/UpdateCoffee";
+export type { ValidationError } from "./models/ValidationError";
 
-export { CoffeeImagesService } from './services/CoffeeImagesService';
-export { CoffeesService } from './services/CoffeesService';
-export { HealthService } from './services/HealthService';
-export { RatingsService } from './services/RatingsService';
+export { CoffeeImagesService } from "./services/CoffeeImagesService";
+export { CoffeesService } from "./services/CoffeesService";
+export { HealthService } from "./services/HealthService";
+export { RatingsService } from "./services/RatingsService";
