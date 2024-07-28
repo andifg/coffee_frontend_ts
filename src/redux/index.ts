268863b268404b5dbac40d829d94e9f8012a2883
anixel <45008199+andifg@users.noreply.github.com>
2024-07-28 09:01:24 +0200

    remove not longer used redux reducer

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "./UserReducer/UserReducer";
-import GeneralConfigState from "./GeneralConfigReducer/GeneralConfigReducer";
 
 export const store = configureStore({
   reducer: {
     user: UserReducer,
-    generalConfig: GeneralConfigState,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
