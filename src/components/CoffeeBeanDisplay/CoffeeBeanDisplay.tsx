1576900e54710212ce32f12c50dbb801eb3d7953
anixel <45008199+andifg@users.noreply.github.com>
2024-07-30 22:10:52 +0200

    add coffee bean display component

---
+++ src/components/CoffeeBeanDisplay/CoffeeBeanDisplay.tsx
@@ -0,0 +1,34 @@
+import "./CoffeeBeanDisplay.scss";
+import theme from "../../theme";
+import UserAvatar from "../UserAvatar/UserAvatar";
+
+type CoffeeBeanProps = {
+  coffeeBeanName: string;
+  roastingCompany: string;
+  ownerName?: string;
+};
+
+const CoffeeBeanDisplay = (props: CoffeeBeanProps) => {
+  return (
+    <div className="coffee-bean-display">
+      {props.ownerName && (
+        <div className="coffee-bean-display-user-avatar">
+          <UserAvatar given_name={props.ownerName} />
+        </div>
+      )}
+      <div className="coffee-bean-display-names">
+        <div className="coffee-bean-display-bean-name">
+          {props.coffeeBeanName}
+        </div>
+        <div
+          style={{ color: theme.palette.secondary.main }}
+          className="coffee-bean-display-roasting-company"
+        >
+          {props.roastingCompany}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { CoffeeBeanDisplay };
