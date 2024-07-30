3c5a0bc53b9e3a834862b9c4c6af2d7b03b5ec5d
anixel <45008199+andifg@users.noreply.github.com>
2024-07-30 22:13:50 +0200

    add CoffeeDrinksBoard

---
+++ src/components/CoffeeDrinksBoard/useManageCoffeeDrinksState.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState, useRef } from "react";
+import useClientService from "../../hooks/useClientService";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../redux";
+
+import {
+  Rating as CoffeeDrink,
+  RatingsService as CoffeeDrinkService,
+} from "../../client";
+// import { AddCoffeeCallbackContext } from "../AddCoffeeCallbackContext/AddCoffeeCallbackContext";
+import useInfiniteScroll from "../useInfiniteScroll/useInfinteScroll";
+
+function useManageCoffeeDrinksState(): [
+  CoffeeDrink[],
+  () => Promise<void>,
+  boolean,
+  boolean,
+] {
+  const [callClientServiceMethod] = useClientService();
+  const [coffeeDrinks, setCoffeeDrinks] = useState<CoffeeDrink[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // const { setCallback } = useContext(AddCoffeeCallbackContext);
+
+  const page = useRef<number>(0);
+  const infiniteScrollFirstCoffeeDrink = useRef<string | undefined>(undefined);
+
+  // const addCoffee = (coffee: CoffeeSchema) => {
+  //   setCoffees((prevState) => [coffee, ...prevState]);
+  // };
+
+  // const updateCoffee = (coffee: CoffeeSchema) => {
+  //   const newCoffees = coffees.map((c) => {
+  //     if (c._id === coffee._id) {
+  //       return coffee;
+  //     }
+  //     return c;
+  //   });
+
+  //   setCoffees(newCoffees);
+  // };
+
+  // const deleteCoffee = (coffee_id: string) => {
+  //   const newCoffees = coffees.filter((c) => c._id !== coffee_id);
+  //   setCoffees(newCoffees);
+  // };
+
+  const loadNextPage = async () => {
+    console.log("Load page: ", page.current + 1);
+
+    const newCoffees = await callClientServiceMethod({
+      function: CoffeeDrinkService.listRatingsApiV1RatingsGet,
+      args: [
+        page.current + 1,
+        5,
+        undefined,
+        infiniteScrollFirstCoffeeDrink.current,
+      ],
+    });
+
+    if (newCoffees.length === 0) {
+      console.log("No more data");
+      return Promise.reject("No more data");
+    }
+
+    if (page.current === 0) {
+      setCoffeeDrinks(newCoffees);
+      infiniteScrollFirstCoffeeDrink.current = newCoffees[0]._id;
+    } else {
+      setCoffeeDrinks((prevState) => [...prevState, ...newCoffees]);
+    }
+
+    page.current += 1;
+  };
+
+  const [showInfitescroll, resetTriggered] = useInfiniteScroll({
+    functionToTrigger: loadNextPage,
+  });
+
+  const fetchFirstPage = async () => {
+    page.current = 0;
+    infiniteScrollFirstCoffeeDrink.current = undefined;
+    setLoading(true);
+    try {
+      await loadNextPage();
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+    resetTriggered();
+  };
+
+  useEffect(() => {
+    // setCallback(() => addCoffee);
+    fetchFirstPage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return [coffeeDrinks, fetchFirstPage, loading, showInfitescroll];
+}
+
+export { useManageCoffeeDrinksState };
